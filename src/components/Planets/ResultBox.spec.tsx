b4196b0d99817429c3c108391c0cad9100ebaeb4
David Auza <davidauza01@gmail.com>
2022-03-17 00:32:58 -0500

    Add a select label in the ResultBox component

--- src/components/Planets/ResultBox.spec.tsx
+++ src/components/Planets/ResultBox.spec.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ResultBox from './ResultBox';
 
+const text = 'portrait';
+
 describe('ResultBox component', () => {
   it('correctly renders the text prop', () => {
-    render(<ResultBox text={'portrait'} />);
+    render(<ResultBox text={text} />);
     const label = screen.getByText(/portrait/i);
+    expect(label).toBeInTheDocument();
+  });
+
+  it('renders the select label', () => {
+    render(<ResultBox text={text} />);
+    const label = screen.getByText(/select/i);
     expect(label).toBeInTheDocument();
   });
 });
--- src/components/Planets/ResultBox.tsx
+++ src/components/Planets/ResultBox.tsx
@@ -5,6 +5,7 @@ const ResultBox = ({ text }: { text: string }) => {
   return (
       <div className="d-flex justify-content-between result-box">
         <span className="ms-5 ps-5 text">{text}</span>
+        <span className="me-5 pe-5 text-muted">Select</span>
       </div>
   );
 }
